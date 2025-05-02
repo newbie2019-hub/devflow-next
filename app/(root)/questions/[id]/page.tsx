ccfd8bf3ef63c337d346a4f574f22b02e948328a
Yvan Sabay <sabayyvan2018@gmail.com>
2025-05-03 06:19:24 +0800

    implement vote ui

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -8,6 +8,7 @@ import Preview from '@/components/editor/Preview';
 import AnswerForm from '@/components/forms/AnswerForm';
 import Metric from '@/components/Metric';
 import UserAvatar from '@/components/UserAvatar';
+import Votes from '@/components/votes/Votes';
 import ROUTES from '@/constants/routes';
 import { getAnswers } from '@/lib/actions/answer.action';
 import { getQuestion, incrementViews } from '@/lib/actions/question.action';
@@ -57,7 +58,12 @@ const AskQuestion = async ({ params }: RouteParams) => {
           </div>
 
           <div className="flex justify-end">
-            <p>Votes</p>
+            <Votes
+              upvotes={question.upvotes}
+              hasUpvoted={true}
+              hasDownvoted={false}
+              downvotes={question.downvotes}
+            />
           </div>
         </div>
 
